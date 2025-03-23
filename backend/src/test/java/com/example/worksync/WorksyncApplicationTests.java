6bbb2109b7ba655ba79a8205a13a26de15c12865
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2025-03-23 12:03:23 -0300

    mend

--- backend/src/test/java/com/example/worksync/WorksyncApplicationTests.java
+++ backend/src/test/java/com/example/worksync/WorksyncApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.worksync;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class WorksyncApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(true).isTrue();
 	}
 
 }
